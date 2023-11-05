3571393a074331bc7b7a7ceca35e7b4010011fc7
Nikita Sivakov <sivakov512@icloud.com>
2023-11-05 01:51:12 +0100

    Make Clippy happy

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,35 @@ use bsp::entry;
 use defmt_rtt as _;
 use panic_probe as _;
 
+use bsp::hal::{
+    clocks::{init_clocks_and_plls, Clock},
+    pac,
+    watchdog::Watchdog,
+};
+
+const DELAY_MS: u32 = 1_000;
+
 #[entry]
 fn main() -> ! {
-    loop {}
+    let mut pac = pac::Peripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
+    let mut watchdog = Watchdog::new(pac.WATCHDOG);
+
+    let clocks = init_clocks_and_plls(
+        bsp::XOSC_CRYSTAL_FREQ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
+
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+
+    loop {
+        delay.delay_ms(DELAY_MS);
+    }
 }
